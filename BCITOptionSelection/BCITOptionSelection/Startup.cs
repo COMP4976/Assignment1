87b65e822333f37157a323cb80b86b195734fc33
JessicaSaini <jessicasaini@hotmail.com>
2016-02-04 21:01:04 +0000

    adding files

---
+++ BCITOptionSelection/BCITOptionSelection/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BCITOptionSelection.Startup))]
+namespace BCITOptionSelection
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
